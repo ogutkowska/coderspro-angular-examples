e2d71adf144e1b94f31c92290fae6332a0985e22
ogutkowska <o.gutkowskaz@gmail.com>
2017-04-02 16:43:55 +0200

    nie dziala

--- app.js
+++ app.js
@@ -3,28 +3,66 @@
 var countryApp = angular.module('countryApp', [
   'ngRoute'
 ]);
-
 countryApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'country-list.html',
-      controller: 'CountryListCtrl'
-    })
-    .when('/:countryName', {
-      templateUrl: 'country-detail.html',
-      controller: 'CountryDetailCtrl'
-    })
-    .otherwise({
-      redirect: '/'
-    })
+    $routeProvider
+        .when('/', {
+            templateUrl: 'country-list.html',
+            controller: 'CountryListCtrl'
+        })
+        .when('/:countryId', {
+            templateUrl: 'country-detail.html',
+            controller: 'CountryDetailCtrl'
+        })
+        .otherwise({
+            redirect: '/'
+        })
 }]);
 
-countryApp.controller('CountryListCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('countries.json').success(function (data) {
-      $scope.countries = data;
+countryApp.factory('countries', ['$http', function($http) {
+    return {
+        list: function (callback) {
+            $http.get({
+                method: 'GET',
+                url: 'countries.json',
+                cache: true
+            }).success(callback)
+        };
+},
+    find: function (id, callback) {
+
+        $http({
+            method: 'GET',
+            url: "country_" + id + ".json",
+            cache: true
+        }).success(callback)
+    };
+};
+
+
+
+   }
+}]);
+
+
+countryApp.controller('CountryListCtrl', ['$scope', '$http', 'countries',
+function($scope,countries) {
+  countries.list(function(countries){
+   $scope.countries = countries;
+  })
+
+}]);
+
+countryApp.controller('CountryDetailCtrl', ['$scope', '$routeParams','$http',
+function ($scope, $routeParams, $http) {
+    countries.find($routeparams.countryId, function(country){
+        $scope.country = country;
     });
-}]);
+    });
 
-countryApp.controller('CountryDetailCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
-  $scope.name - $routeParams.countryName;
+
+
+
+    $scope.goBack = function () {
+      window.history.back();
+    }
 }]);
